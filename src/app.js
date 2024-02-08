906907fc0e79d51af9bdc897f0c7beb8e51ef89f
Katrina Pingryn <katep@indeed.com>
2024-02-07 19:05:16 -0600

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use(express.json());
+
+app.use("/pastes", pastesRouter); // Note: app.use
+app.use("/users", usersRouter);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +14,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
